7eec7261b2847e7d472781fd2f8885ed9a8cde14
Venera Osombaeva <osomka.v@gmail.com>
2024-04-26 12:13:44 +0600

    Создан родительский класс PaymentMethod для разных способов оплаты

---
+++ src/model/PaymentMethod.java
@@ -0,0 +1,17 @@
+package model;
+
+public class PaymentMethod {
+    private int amount;
+
+    public PaymentMethod(int amount) {
+        this.amount = amount;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
+}
